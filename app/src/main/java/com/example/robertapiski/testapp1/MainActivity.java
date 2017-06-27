7e530aeb7ab001bbab9c086fb3ffc0760c63f8cf
Robert <robert.lapinski@outlook.com>
2017-06-27 17:39:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/robertapiski/testapp1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.robertapiski.testapp1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> projects;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        projects = new ArrayList<String>();
+    }
+
+    public void addProject(View v) {
+        final ListView projectsList = (ListView) findViewById(R.id.projectsList);
+
+        projects.add("aaaa");
+
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, projects);
+        projectsList.setAdapter(adapter);
+    }
+}
